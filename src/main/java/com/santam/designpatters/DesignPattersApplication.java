35afa3a8730a290f2fb38ba5d1833e5ca56bd4eb
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-02-29 13:40:45 -0300

    first commit

---
+++ src/main/java/com/santam/designpatters/DesignPattersApplication.java
@@ -0,0 +1,13 @@
+package com.santam.designpatters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPattersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPattersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santam/designpatters/DesignPattersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santam.designpatters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPattersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
